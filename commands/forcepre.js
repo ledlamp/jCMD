a25bdd98bc20f08748fd416169ff31112ca877d4
recapitalverb <jackredstonia64@gmail.com>
2019-03-04 22:28:28 +0700

    More patches

--- commands/forcepre.js
+++ commands/forcepre.js
@@ -1,9 +1,18 @@
 module.exports = {
 	run: async function (msg, p) {
 		let cf = client.data.guilds.get(msg.guild.id) || {}
-		if (!p[0]) delete cf.prefix
+		if (!p[0]) {
+			delete cf.prefix
+			client.data.writeGuild(msg.guild.id, cf)
+			return {
+				content: `Prefix reset.`
+			}
+		}
 		else cf.prefix = p[0]
 		client.data.writeGuild(msg.guild.id, cf)
+		return {
+			content: `Prefix set to \`${p[0]}\`.`
+		}
 	},
 	cat: 'maint',
 	reqGuild: true,
