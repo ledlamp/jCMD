638da54b0ecb8b459a1951d3d2ae1e1e1c8876ea
recapitalverb <jackredstonia64@gmail.com>
2019-03-10 22:32:19 +0700

    Category

--- commands/pasta.js
+++ commands/pasta.js
@@ -2,5 +2,6 @@ module.exports = {
 	run: async function (msg) {
 		return {content: client.lang.paste()}
 	},
+	cat: 'fun',
 	desc: 'Spits out a copypasta that shows up when ya haxxors try doing `eval`.'
 }
