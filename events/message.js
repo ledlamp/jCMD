934fec9df623fd95f36605a9aae93f1d64d67102
recapitalverb <jackredstonia64@gmail.com>
2019-03-11 20:41:48 +0700

    Cascading permission support

--- events/message.js
+++ events/message.js
@@ -34,11 +34,15 @@ module.exports = async function (msg) {
 	if (!cmdst) return
 	if (client.cd.has(msg.author.id)) return msg.channel.send('Slow down, take it easy.').catch(()=>undefined)
 	function deepCmd(obj, p, his) {
+		let noParse = false
 		if (obj.own && (msg.author.id !== client.config.ownerID)) return client.util.throw(msg, client.lang.paste())
 		if (obj.reqGuild && isDM) return client.util.throw(msg, 'You can only do that command in a server text channel.')
 		if (obj.nsfw && !msg.channel.nsfw) return client.util.throw(msg, 'You can only do that command in a NSFW channel.')
+		if (obj.args && (p.length < obj.args.length)) noParse = true
+		if (!noParse && obj.perm && !msg.channel.permissionsFor(msg.member).has(obj.perm)) return client.util.throw(msg, 'Insufficient permissions. You are missing at least one of: `' + client.util.permName(obj.perm) + '`.')
+		if (obj.botPerm && !msg.channel.permissionsFor(msg.guild.member(client.user)).has(obj.perm)) return client.util.throw(msg, 'Insufficient permissions for the bot. The bot is missing at least one of: `' + client.util.permName(obj.botPerm) + '`.')
 		if (obj.run) {
-			if (obj.args && (p.length < obj.args.length)) {
+			if (noParse) {
 				if (!obj.noParse) return client.util.throw(msg, 'Not enough arguments. Arguments needed: ' + client.util.argSq(obj.args))
 				else return obj.noParse(msg)
 				.then (function (resp) {
@@ -53,8 +57,6 @@ module.exports = async function (msg) {
 					}
 				})
 			}
-			if (obj.perm && !msg.channel.permissionsFor(msg.member).has(obj.perm)) return client.util.throw(msg, 'Insufficient permissions. You are missing at least one of: `' + client.util.permName(obj.perm) + '`.')
-			if (obj.botPerm && !msg.channel.permissionsFor(msg.guild.member(client.user)).has(obj.perm)) return client.util.throw(msg, 'Insufficient permissions for the bot. The bot is missing at least one of: `' + client.util.permName(obj.botPerm) + '`.')
 			obj.run(msg, args)
 			.then (function (resp) {
 				if (resp && (resp.content || resp.options))
@@ -63,14 +65,15 @@ module.exports = async function (msg) {
 			.catch (function (err) {
 				if (err instanceof UserInputError) client.util.throw(msg, err.toString())
 				else {
-					client.users.get(client.config.ownerID).send(`UNEXPECTED ERROR OCCURED\nMESSAGE CONTENT: \`\`\`${msg.content}\`\`\`\nCOMMAND EXECUTED: \`${his}\`\nERROR:\`\`\`${util.inspect(err)}\`\`\``)
+					client.users.get(client.config.ownerID).send(`UNEXPECTED ERROR OCCURED\nMESSAGE CONTENT: \`\`\`${msg.content}\`\`\`\nCOMMAND EXECUTED: \`${his}\`\nERROR:\n\`\`\`${util.inspect(err)}\`\`\``)
 					client.util.throw(msg, 'Ouch! jCMD has encountered an unexpected error, and it has been automatically reported to the bot developer. Thank you for your cooperation!')
 				}
 			})
 			if(msg.author.id !== client.config.ownerID) client.cd.addCooldown(msg.author.id, obj.cd)
 		}
 		else {
-			if (!p[0]) return client.util.throw(msg, `You need to define a subcommand. Do \`${prefix}help ${his}\` for more information.`)
+			//if (!p[0]) return client.util.throw(msg, `You need to define a subcommand. Do \`${prefix}help ${his}\` for more information.`)
+			if (!p[0]) return client.commands.get('help').run(msg, his.split(' ')).then(({content, options}) => client.util.done(msg, content, options))
 			let subc = p.shift()
 			let clip = subc.length > 15 ? subc.slice(0, 15) + '...' : subc // Prevent user from entering long subcommands and making the bot spam
 			if (Object.keys(obj.subCmd).indexOf(subc) == -1) return client.util.throw(msg, `Invalid subcommand \`${clip}\`. Do \`${prefix}help ${his}\` for more information.`)
