c44c37bd85846d374a6db7401a4892fdbe352022
recapitalverb <jackredstonia64@gmail.com>
2019-03-11 20:41:34 +0700

    Bugfix for DM support

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,7 +1,7 @@
 module.exports = {
 	run: async function (msg, args) {
 		let ch = client.channels.get(client.config.suggestChID)
-		if (ch) ch.send(`\`\`\`${args.join(' ')}\`\`\`\nSuggested by **${msg.author.tag}** in **${msg.guild.name}**.`)
+		if (ch) ch.send(`\`\`\`${args.join(' ')}\`\`\`\nSuggested by **${msg.author.tag}**${msg.channel.type === 'text' ? ' in **' + msg.guild.name + '**' : ''}.`)
 		return {content: 'Suggestion sent successfully.'}
 	},
 	cat: 'info',
