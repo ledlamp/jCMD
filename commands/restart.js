72955cd734bf99afe5f3726e31ce99dc4900af29
recapitalverb <jackredstonia64@gmail.com>
2019-03-16 20:04:26 +0700

    Destroy client before exiting

--- commands/restart.js
+++ commands/restart.js
@@ -1,20 +1,9 @@
 module.exports = {
 	run: async function (msg, p) {
-		const resID = client.config.ownerID
-		let check = true, csl = '', buf = undefined
-		try {
-			csl = String(require('child_process').spawn('pm2', ['restart','index']))
-			if (csl.length > 1800) buf = Buffer.from(csl)
-		} catch (err) {
-			check = false
-			csl = err
-		}
-		return {
-			content: (check ? '**Returned from spawn function:**' : '**Error occured:**') + (buf ? '' : '\n```js\n' + csl + '```'),
-			options: buf ? new Discord.Attachment(buf, 'output.txt') : undefined
-		}
+		msg.channel.send('See you later. Hopefully. ;)').catch(()=>undefined)
+		client.destroy().then(()=>require('child_process').spawn('pm2', ['restart','index'])).catch(()=>undefined)
 	},
 	cat: 'maint',
 	own: true,
-	desc: 'Restart the bot.'
+	desc: 'Destroys the client object then restarts the bot.'
 }
