5480dbf5f5792edbdd116fb7fdebaa7ca9b47f80
recapitalverb <jackredstonia64@gmail.com>
2019-03-16 20:03:17 +0700

    start/stopTyping

--- commands/waifu.js
+++ commands/waifu.js
@@ -1,8 +1,10 @@
 module.exports = {
 	run: async function (msg) {
+		msg.channel.startTyping()
 		return fetch('http://api.cutegirls.moe/json').then(resp => resp.json()).then(function (obj) {
+			msg.channel.stopTyping()
 			if (obj.status === 200) return {
-				options: {embed: client.util.mkEmbed(`**${obj.data.title}** by **${obj.data.author}** from r/**${obj.data.sub}**`, undefined, undefined, obj.data.image)}
+				options: {embed: client.util.mkEmbed(undefined, `**${obj.data.title}** by **${obj.data.author}** from r/**${obj.data.sub}**`, undefined, obj.data.image)}
 			}
 			else {
 				throw new UserInputError(`HTTP status of cutegirls API was: ` + obj.status)
