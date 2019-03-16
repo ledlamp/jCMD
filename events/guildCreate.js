e02dd8218f762b8a10989e64225182e541de2ca3
recapitalverb <jackredstonia64@gmail.com>
2019-03-16 20:04:55 +0700

    Avoid emitting event on every guild upon restart

--- events/guildCreate.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function (guild) {
-	let ch = guild.channels.find(function (channel) {return channel.name.startsWith('general') || channel.name.startsWith('main')})
-	if (ch) ch.send(`Thank you for using jCMD!\nMy prefix is \`${client.config.prefix}\`, and you can always configure it anytime. To get started, use the \`${client.config.prefix}help\` command. Additional information for a particular command can be shown by doing \`${client.config.prefix}help [command]\`.\nIf you need help, join our Discord server: https://discord.gg/K9EfuXZ\nHope you have fun!`)
-}
