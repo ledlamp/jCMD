5d0fe05f6088c72d5960695bf39a4ce712bd2e04
recapitalverb <jackredstonia64@gmail.com>
2019-03-28 21:23:45 +0700

    Language

--- commands/ping.js
+++ commands/ping.js
@@ -2,10 +2,10 @@ module.exports = {
 	run: async function (msg) {
 		msg.channel.send('Ping...')
 		.then(function (m) {
-			m.edit(`Ping... pong! Latency: ${m.createdTimestamp - msg.createdTimestamp}ms, API latency: ${Math.round(client.ping)}ms.`).catch(()=>undefined)
+			m.edit(`Ping... pong! API latency: ${m.createdTimestamp - msg.createdTimestamp}ms, Websocket latency: ${Math.round(client.ping)}ms.`).catch(()=>undefined)
 		})
 		.catch(()=>undefined)
 	},
 	cat: 'util',
-	desc: 'Measures then shows the bot\'s Internet connection\'s latency. Normally, the values should be between 50 and 400 milliseconds.'
+	desc: 'Measures then shows the bot\'s Internet connection\'s latency..'
 }
