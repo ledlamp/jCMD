a1a87e3af494582fe95daea67397eb6babf897b8
recapitalverb <jackredstonia64@gmail.com>
2019-03-31 19:34:21 +0700

    Recursively delete messages for autodelete

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,8 +2,25 @@ module.exports = function (member) {
 	let cf = client.data.guilds.get(member.guild.id)
 	if (cf && cf.autoDel) cf.autoDel.map(function (id) {
 		let ch = member.guild.channels.get(id)
-		if (ch) ch.fetchMessages().then(function (messages) {
-			ch.bulkDelete(messages.filter(function (message) {return message.author.id === member.user.id})).catch(()=>undefined)
-		})
+		if (ch && ch.permissionsFor(ch.guild.me).has('MANAGE_MESSAGES')) {
+			let fetched = [], x = 0, id = ''
+			function fetcher () {
+				ch.fetchMessages({before: id || undefined})
+				.then(function (msgs) {
+					if (x > 5) {
+						return fetched.map(function (coll) {
+							ch.bulkDelete(coll, true).catch(()=>undefined)
+						})
+					}
+					else {
+						x++
+						fetched.push(msgs.filter(function (message) {return message.author.id === member.user.id}))
+						id = msgs.last().id
+						fetcher()
+					}
+				})
+			}
+			fetcher()
+		}
 	})
 }
