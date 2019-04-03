b8007ae890429e037b3120e269b0d2f1e6889e09
recapitalverb <jackredstonia64@gmail.com>
2019-04-03 23:26:47 +0700

    Allow users to get daily at any time in the day

--- commands/daily.js
+++ commands/daily.js
@@ -1,8 +1,11 @@
+function morning(time) {
+	return time - (time % 86400000)
+}
 module.exports = {
 	run: async function (msg) {
-		let d = client.data.users.get(msg.author.id) || {}, now = Date.now()
-		if (d.daily && now - d.daily < 86400000) throw new UserInputError(`You can only collect daily rewards once per day, mate. Try again in ${Math.ceil(24 - (now - d.daily) / 3600000)} hours.`)
-		d.daily = now
+		let d = client.data.users.get(msg.author.id) || {}, n = Date.now(), m = morning(n)
+		if (d.daily > m) throw new UserInputError(`You can only collect daily rewards once per day, mate. Try again in ${Math.ceil(24 - (n - m) / 3600000)} hours.`)
+		d.daily = n
 		if (!d.bal) d.bal = 0
 		d.bal += 200
 		client.data.writeUser(msg.author.id, d)
