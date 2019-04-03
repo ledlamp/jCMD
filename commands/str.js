ff54444cbc3c875003cf530ce15caa61c1c7605d
recapitalverb <jackredstonia64@gmail.com>
2019-04-03 23:27:06 +0700

    Comply with newer command handler

--- commands/str.js
+++ commands/str.js
@@ -1,84 +1,87 @@
 let alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],regAr=['🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯','🇰','🇱','🇲','🇳','🇴','🇵','🇶','🇷','🇸','🇹','🇺','🇻','🇼','🇽','🇾','🇿']
 module.exports = {
 	run: async function (msg, p) {
-		let text = p.join(' '), ms = undefined
-		client.util.done(msg, 'Alright, I got the text. Now tell me what methods you want to apply to it. Keep in mind that you can see all available methods using the `help` command, and you only have 20 seconds.').then(function (mes) {ms = mes})
+		let text = p.join(' ')
 		return new Promise(function (res, rej) {
-			let f = function (message) {
-				if (message.channel.id === msg.channel.id && message.author.id === msg.author.id) {
-					client.off('message', f)
-					client.clearTimeout(t)
-					let meth = message.content.split(' ')
-					if (meth.length > 10) rej(new UserInputError('Too many methods were given! (> 10)'))
-					while(meth[0]) {
-						let s = meth.shift(), c = s.split('-')
-						let pn = parseInt(c[1])
-						if (isNaN(pn)) pn = 1
-						if (pn > 10) pn = 10
-						switch(c[0]) {
-						case 'aest':
-							text = [...text].join(' '.repeat(pn))
-							break
-						case 'clap':
-							text = text.split(' ').join(' 👏 ')
-							break
-						case 'rev':
-							text = [...text].reverse().join('')
-							break
-						case 'upper':
-							text = text.toUpperCase()
-							break
-						case 'lower':
-							text = text.toLowerCase()
-							break
-						case 'ita':
-							text = `*${text}*`
-							break
-						case 'bold':
-							text = `**${text}**`
-							break
-						case 'code':
-							text = `\`${text}\``
-							break
-						case 'codeblock':
-							text = `\`\`\`${text}\`\`\``
-							break
-						case 'drag':
-							text = [...text]
-							let drag = ''
-							for (let i of text) drag += i.repeat(pn)
-							text = drag
-							break
-						case 'circle':
-							text = text.toLowerCase().replace(/[a-z]/g, function(char) {
-								return String.fromCharCode(char.charCodeAt(0) + 9327)
-							})
-							break
-						case 'regind':
-							let regind = ''
-							text = [...text]
-							for (let x = 0; x < text.length; x++) regind += (regAr[alph.indexOf(text[x].toLowerCase())] ? (regAr[alph.indexOf(text[x-1] ? text[x-1].toLowerCase() : undefined)] ? '' : ' ') + regAr[alph.indexOf(text[x].toLowerCase())] + ' ' : text[x])
-							text = regind.trim()
-							break
-						case 'mock':
-							let mock = ''
-							text = [...text]
-							for (let x = 0; x < text.length; x++) mock += (text[x] === 'i' ? 'i' : Math.round(Math.random()) === 1 ? text[x].toUpperCase() : text[x].toLowerCase())
-							text = mock
-							break
+			client.util.done(msg, 'Alright, I got the text. Now tell me what methods you want to apply to it. Keep in mind that you can see all available methods using the `help` command, and you only have 20 seconds.').then(function (ms) {
+				let f = function (message) {
+					if (message.channel.id === msg.channel.id && message.author.id === msg.author.id) {
+						client.off('message', f)
+						client.clearTimeout(t)
+						let meth = message.content.split(' ')
+						if (meth.length > 10) rej(new UserInputError('Too many methods were given! (> 10)'))
+						while(meth[0]) {
+							let s = meth.shift(), c = s.split('-')
+							let pn = parseInt(c[1])
+							if (isNaN(pn)) pn = 1
+							if (pn > 10) pn = 10
+							switch(c[0]) {
+							case 'aest':
+								text = [...text].join(' '.repeat(pn))
+								break
+							case 'clap':
+								text = text.split(' ').join(' 👏 ')
+								break
+							case 'rev':
+								text = [...text].reverse().join('')
+								break
+							case 'upper':
+								text = text.toUpperCase()
+								break
+							case 'lower':
+								text = text.toLowerCase()
+								break
+							case 'ita':
+								text = `*${text}*`
+								break
+							case 'bold':
+								text = `**${text}**`
+								break
+							case 'code':
+								text = `\`${text}\``
+								break
+							case 'codeblock':
+								text = `\`\`\`${text}\`\`\``
+								break
+							case 'drag':
+								text = [...text]
+								let drag = ''
+								for (let i of text) drag += i.repeat(pn)
+								text = drag
+								break
+							case 'circle':
+								text = text.toLowerCase().replace(/[a-z]/g, function(char) {
+									return String.fromCharCode(char.charCodeAt(0) + 9327)
+								})
+								break
+							case 'regind':
+								let regind = ''
+								text = [...text]
+								for (let x = 0; x < text.length; x++) regind += (regAr[alph.indexOf(text[x].toLowerCase())] ? (regAr[alph.indexOf(text[x-1] ? text[x-1].toLowerCase() : undefined)] ? '' : ' ') + regAr[alph.indexOf(text[x].toLowerCase())] + ' ' : text[x])
+								text = regind.trim()
+								break
+							case 'mock':
+								let mock = ''
+								text = [...text]
+								for (let x = 0; x < text.length; x++) mock += (text[x] === 'i' ? 'i' : Math.round(Math.random()) === 1 ? text[x].toUpperCase() : text[x].toLowerCase())
+								text = mock
+								break
+							}
+							if (text.length > 800) text = text.substring(0, 800)
 						}
-						if (text.length > 800) text = text.substring(0, 800)
+						text = client.util.clean(text)
+						ms.delete().catch(()=>undefined)
+						res({content: text, traces: [message]})
 					}
-					text = client.util.clean(text)
-					if (ms) ms.delete().catch(()=>undefined)
-					res({content: text})
 				}
-			}
-			let t = client.setTimeout(function () {
-				client.off('message', f)
-				rej(new UserInputError('Timeout! You took too long to reply.'))
-			}, 20000)
-			client.on('message', f)
+				let t = client.setTimeout(function () {
+					client.off('message', f)
+					rej(new UserInputError('Timeout! You took too long to reply.'))
+				}, 20000)
+				client.on('message', f)
+			}).catch(function () {
+				res(undefined)
+			})
 		})
 	},
 	cat: 'strp',
