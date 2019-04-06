6adf774a2fa834fcc0a8a558a36b1831e9cfdb65
recapitalverb <jackredstonia64@gmail.com>
2019-04-06 19:02:00 +0700

    Bugfix: garbage collector not writing

--- events/ready.js
+++ events/ready.js
@@ -4,16 +4,6 @@ module.exports = function () {
 	Promise.all([client.data.users.defer, client.data.guilds.defer])
 	.then(function () {
 		console.log(`Data: loaded ${client.data.users.size} user entries, ${client.data.guilds.size} guild entries.`)
-		// Load events
-		fs.readdir('./events/', function (err, files) {
-			if (err) return console.error(err)
-			files.map(function (file) {
-				if (!file.endsWith('.js') || file === 'ready.js') return
-				const event = require(`./${file}`)
-				let eventName = file.split('.')[0]
-				client.on(eventName, event)
-			})
-		})
 		client.data.guilds.map(function (v, k) {
 			if (!client.guilds.has(k)) return client.data.guilds.delete(k)
 			let guild = client.guilds.get(k)
@@ -24,6 +14,17 @@ module.exports = function () {
 			if (v.invScan) v.invScan.map(function (ch) {
 				if (!guild.channels.has(ch)) v.invScan.splice(v.invScan.indexOf(ch), 1)
 				if (v.invScan.length === 0) delete v.invNoti
+			})
+			client.data.writeGuild(k, v)
+		})
+		// Load events
+		fs.readdir('./events/', function (err, files) {
+			if (err) return console.error(err)
+			files.map(function (file) {
+				if (!file.endsWith('.js') || file === 'ready.js') return
+				const event = require(`./${file}`)
+				let eventName = file.split('.')[0]
+				client.on(eventName, event)
 			})
 		})
 	})
