869d2bf80b821d7a057303a9b8c0e52a61235222
recapitalverb <jackredstonia64@gmail.com>
2019-04-13 22:39:35 +0700

    Links in title

--- commands/waifu.js
+++ commands/waifu.js
@@ -4,7 +4,7 @@ module.exports = {
 		return fetch('http://api.cutegirls.moe/json').then(resp => resp.json()).then(function (obj) {
 			msg.channel.stopTyping()
 			if (obj.status === 200) return {
-				options: {embed: client.util.mkEmbed(undefined, `**${obj.data.title}** by **${obj.data.author}** from r/**${obj.data.sub}**`, undefined, obj.data.image)}
+				options: {embed: client.util.mkEmbed(undefined, `[**${obj.data.title}**](${obj.data.link}) by [u/**${obj.data.author}**](https://reddit.com/u/${obj.data.author}) from [r/**${obj.data.sub}**](https://reddit.com/r/${obj.data.sub})`, undefined, obj.data.image)}
 			}
 			else {
 				throw new UserInputError(`HTTP status of cutegirls API was: ` + obj.status)
