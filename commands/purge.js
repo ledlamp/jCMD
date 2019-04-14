25ec97e325d184bd7f7348753c8bc282a9a10fa0
recapitalverb <jackredstonia64@gmail.com>
2019-04-14 20:33:46 +0700

    Purge up to 1k messages

--- commands/purge.js
+++ commands/purge.js
@@ -1,13 +1,41 @@
 module.exports = {
 	run: async function(msg, p) {
 		let deleteCount = parseInt(p[0]) + 1
-		if (isNaN(deleteCount) || deleteCount < 2 || deleteCount > 100) throw new UserInputError('Please provide a number between 1 and 99 for the number of messages to delete.')
-		return msg.channel.bulkDelete(deleteCount, true).catch(err => {throw new UserInputError(`Couldn't delete messages. Error: ${err.message}`)})
+		if (isNaN(deleteCount) || deleteCount < 2 || deleteCount > 1000) throw new UserInputError('Please provide a number between 1 and 99 for the number of messages to delete.')
+		function deleter () {
+			if (deleteCount > 100) {
+				deleteCount -= 100
+				return msg.channel.fetchMessages({limit: 100})
+				.then(function (msgs) {
+					console.log(msgs.size)
+					return msg.channel.bulkDelete(msgs, true)
+					.then(deleter)
+					.catch(function (e) {
+						throw e
+					})
+				})
+				.catch(function (e) {
+					return {
+						content: 'Couldn\'t delete messages. ' + e.toString()
+					}
+				})
+			} else {
+				return msg.channel.fetchMessages({limit: deleteCount})
+				.then(function (msgs) {
+					console.log(msgs.size)
+					return msg.channel.bulkDelete(msgs, true)
+					.then(function () {
+						return undefined
+					})
+				})
+			}
+		}
+		return deleter()
 	},
 	cat: 'mod',
 	reqGuild: true,
 	perm: 'MANAGE_MESSAGES',
 	botPerm: 'MANAGE_MESSAGES',
-	args: ['number, 1 -> 99'], argCount: 1,
+	args: ['number, 1 -> 999'], argCount: 1,
 	desc: 'Deletes the most recent (number) messages, plus the message containing the command.'
 }
