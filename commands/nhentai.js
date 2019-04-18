74435ce959a82df090ebec90c5f464a4c7cccad8
recapitalverb <jackredstonia64@gmail.com>
2019-04-18 22:48:07 +0700

    Code organisation

--- commands/nhentai.js
+++ commands/nhentai.js
@@ -1,5 +1,8 @@
-function extractTags(body) { return body.replace(/(\n+([ \t])*)/g, '').match(/<div class="tag-container field-name ">Tags:(.*?)<\/div>/)[1].replace(/\((\d|,)*?\)/g, '').replace(/<.*?>/g, ';').split(/ *;+/).slice(1, -1) }
-function extractLangs(body) { return body.replace(/(\n+([ \t])*)/g, '').match(/<div class="tag-container field-name ">Languages:(.*?)<\/div>/)[1].replace(/\((\d|,)*?\)/g, '').replace(/<.*?>/g, ';').split(/ *;+/).slice(1, -1) }
+// This ultra super spaghetti code is intentionally left uncommented, because hell do I not have the courage to do so
+
+function extractTags(body) { return body.match(/<div class="tag-container field-name ">Tags:(.*?)<\/div>/)[1].replace(/\((\d|,)*?\)/g, '').replace(/<.*?>/g, ';').split(/ *;+/).slice(1, -1) }
+function extractLangs(body) { return body.match(/<div class="tag-container field-name ">Languages:(.*?)<\/div>/)[1].replace(/\((\d|,)*?\)/g, '').replace(/<.*?>/g, ';').split(/ *;+/).slice(1, -1) }
+function extractArtists(body) { return body.match(/<div class="tag-container field-name ">Artists:(.*?)<\/div>/)[1].replace(/\((\d|,)*?\)/g, '').replace(/<.*?>/g, ';').split(/ *;+/).slice(1, -1) }
 function extractTitle(body) { return body.match(/<meta itemprop="name" content="(.+)" \/>/)[1] }
 function similarity(s1, s2) {
 	let longer = s1
@@ -8,11 +11,10 @@ function similarity(s1, s2) {
 		longer = s2
 		shorter = s1
 	}
-	let longerLength = longer.length
-	if (longerLength === 0) {
+	if (longer.length === 0) {
 		return 1.0
 	}
-	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)
+	return (longer.length - editDistance(longer, shorter)) / parseFloat(longer.length)
 }
 function editDistance(s1, s2) {
 	s1 = s1.toLowerCase()
@@ -33,6 +35,7 @@ function editDistance(s1, s2) {
 	}
 	return costs[s2.length]
 }
+
 const forbiddenTitles = [
 	'EMERGENCE',
 	'Legend of The Meme Queen',
@@ -46,7 +49,8 @@ const forbiddenTitles = [
 const cultureTags = [
 	'armpit sex',
 	'bdsm',
-	'vanilla'
+	'vanilla',
+	'leg lock'
 ]
 
 module.exports = {
@@ -57,9 +61,14 @@ module.exports = {
 		let res = await fetch('https://nhentai.net/g/' + code)
 		msg.channel.stopTyping()
 		if (res.status !== 200) throw new UserInputError('Doujinshi not found. Check whether you have given the correct name.')
-		let body = await res.text()
+		let
+		body = await res.text().replace(/(\n+([ \t])*)/g, ''),
+		title = extractTitle(body),
+		tags = extractTags(body), saidTags = false
+		langs = extractLangs(body),
+		artists = extractArtists(body)
+		response = ''
 
-		let title = extractTitle(body), tags = extractTags(body), langs = extractLangs(body), response = '', saidTags = false
 		let abandon = false
 		for (let i of forbiddenTitles) if (title === i || title.startsWith(i)) {
 			abandon = true
@@ -71,7 +80,8 @@ module.exports = {
 			culture.push(i)
 		}
 		let isLoli = tags.includes('lolicon')
-		let ara = tags.includes('shotacon') && tags.includes('big breasts')
+		let ara = tags.includes('shotacon')
+
 		if (code !== '177013' && similarity('177013', code) > 0.8 && isLoli) response += '\nDid you perchance mean **177013** for some good, clean, non-loli fun?\nBut I\'ve got to do my job, so...\n'
 		if (isLoli) {
 			response += `\n**Tags: *lolicon***\n	${abandon || isCultured || ara ? 'FBI OPE--' : 'FBI OPEN UP!'}`
@@ -81,8 +91,14 @@ module.exports = {
 			response += `\n**${saidTags ? 'Also tags' : 'Tags'}: *${culture.join(', ')}***\n	I see you are a man of culture as well.`
 			saidTags = true
 		}
+		if (artists.includes('michiking')) {
+			response += `\n**Artists: *michiking***\n	oh ho you are indeed a man of culture cOUGH COUGH`
+		}
 		if (abandon) response += `\nWait...\n**Title: *${title}***\n	Abandon all hope, ye who enter here`
-		if (ara) response += `\n**${saidTags ? 'Also tags' : 'Tags'}: *big breasts, shotacon***\n	***Ahhhh yes. Vvvveeeerrryyy yyeeeeeeeessss.***`
+		if (ara) {
+			response += `\n**${saidTags ? 'Also tags' : 'Tags'}: *shotacon***\n	***Ahhhh yes. Vvvveeeerrryyy yyeeeeeeeessss.***`
+			saidTags = true
+		}
 		if (!langs.includes('english') && isLoli) response += '\n**Languages: *Not English***\n	Does it look like that I can read moon runes? No matter where the hell you live, here in \'murica we protect the lolis. Take them away boys!'
 		if (response === '') response += '\nOh. Oh wow. Something actually normal this time. I am proud that you have such a fine taste in this wild realm of art. Or maybe you are just a normie, I don\'t know. ¯\\_(ツ)_/¯'
 		return {content: `\n*\`\`\`${code}\`\`\`*` + response}
